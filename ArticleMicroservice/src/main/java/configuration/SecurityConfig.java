da82ae10d26935c8cf35551301844ad191e1d193
marsonto <marius.sontopski@web.de>
2019-04-17 16:58:46 +0200

    Login Form only with default password TODO: add custom user

--- ArticleMicroservice/src/main/java/configuration/SecurityConfig.java
+++ ArticleMicroservice/src/main/java/configuration/SecurityConfig.java
@@ -3,23 +3,21 @@ package configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("ADMIN");
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication().withUser("admin").password("hallo").roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").hasRole("ADMIN")
-                .and()
-            .httpBasic();
+                .antMatchers("/").hasRole("USER")
+                    .and()
+                .httpBasic();
     }
 }
