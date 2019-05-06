9dbe040c04a15a3f69972eb32440c32a64862ec9
marsonto <marius.sontopski@web.de>
2019-05-06 10:03:12 +0200

    Add working security on both ends

--- ShoppingMicorservice/src/main/java/shoppingapp/services/ShoppingService.java
+++ ShoppingMicorservice/src/main/java/shoppingapp/services/ShoppingService.java
@@ -1,5 +1,6 @@
 package shoppingapp.services;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,11 +11,13 @@ public class ShoppingService {
     public Animal getAnimalById(String id) {
         final String uri = "http://localhost:8080/shop";
         final String plain = "dude:wow";
+        byte[] plainBytes = plain.getBytes();
+        byte[] base64Bytes = Base64.encodeBase64(plainBytes);
+        String base64 = new String(base64Bytes);
 
         final HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add(HttpHeaders.USER_AGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36");
-        headers.add(HttpHeaders.AUTHORIZATION, plain);
+        headers.add(HttpHeaders.AUTHORIZATION, "Basic " + base64);
 
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<String> entity = new HttpEntity<>(id, headers);
